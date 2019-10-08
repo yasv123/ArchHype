bd2fbe772d03b46bf986cc3a3936bfbd3b66239b
Yaswin Veluguleti <yaswin.v1@gmail.com>
2019-10-08 18:08:15 -0500

    moar

--- sim/classes.py
+++ sim/classes.py
@@ -7,22 +7,28 @@ class Address:
 class Memory:
     def __init__(self):
         self.mem = defaultdict(int)
+        self.writes = set()
 
     def read(self, address, size):
         result = 0
         origSize = size
+        flag = False
         while size > 0:
             subaddress = address + (origSize - size)
+            if not subaddress in self.writes:
+                flag = True
+                break
             value = self.mem[subaddress]
             value = value << ((size - 1) * 8)
             result += value
             size -= 1
-        return result
+        return (result, flag)
 
     def write(self, address, size, data):
         origSize = size
         while size > 0:
             subaddress = address + (origSize - size)
+            self.writes.add(subaddress)
             value = data >> (size - 1) * 8
             value &= 255
             self.mem[subaddress] = value
--- sim/sim.py
+++ sim/sim.py
@@ -12,18 +12,35 @@ class AccessType(Enum):
     READ = "R"
     WRITE = "W"
 
-def memaccess(ip, access_type, addr, size, mem_at):
+mismatch = {'g': 0, 'f': 0, 'l': 0}
+unmatch = {'g': 0, 'f': 0, 'l': 0}
+
+def memaccess(ip, access_type, addr, size, mem_at, line):
+    global mismatch
+    global unmatch
     # print(str(size) + " byte " + str(AccessType.value) + " at "+str(addr))
     # fill out simulation here using size, AccessType and addr
     if access_type is AccessType.READ:
-        read = mainmem.read(addr, size)
-        if read != mem_at:
-            print('mismatch' + " {0:x} ".format(ip) + "{0:x}".format(addr))
+        read, un = mainmem.read(addr, size)
+        if read != mem_at and not un:
+            mismatch['g'] += 1
+            if line < 37000:
+                mismatch['f'] += 1
+            else:
+                mismatch['l'] += 1
+        elif read != mem_at and un:
+            print('actual', mem_at, 'simulated', read)
+            unmatch['g'] += 1
+            if line < 37000:
+                unmatch['f'] += 1
+            else:
+                unmatch['l'] += 1
     else:
         mainmem.write(addr, size, mem_at)
 
 with open(args.filepath) as f:
     line = f.readline()
+    linenum = 0
     while line:
         if "#" not in line:
             comps = line.split()
@@ -32,5 +49,7 @@ with open(args.filepath) as f:
             addr = int(comps[2], 16)
             size = int(comps[3])
             mem_at = int(comps[4], 16)
-            memaccess(ip, access_type, addr, size, mem_at)
+            memaccess(ip, access_type, addr, size, mem_at, linenum)
         line = f.readline()
+        linenum += 1
+print(mismatch, unmatch)
